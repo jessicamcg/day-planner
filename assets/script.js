7d8e3eaa6c8ebe8568d630466e59b7e878fa612d
jessicamcg <jessicamcguico@gmail.com>
2021-09-30 15:09:35 -0500

    fixed timeblock bg color to respond to actual time

--- assets/script.js
+++ assets/script.js
@@ -1,46 +1,72 @@
 var date = moment().format( 'dddd MMM Do, YYYYY' );
 $('#currentDay').text(date);
 
-for ( var i=9; i<=16; i++) {
+var storedEvent = [];
 
-    var indexTime = '#' + [i];
+function init() {
 
-    if ( i == moment().format('H') ) {
-        $('.col-8').addClass('present');
-    } else if ( i > moment().format('H') ) {
-        $('.col-8').addClass('future');
-    } else if ( i < moment().format('H') ) {
-        $('.col-8').addClass('past');
+    timeColor();
+
+    for (var j=0;j<=17;j++) {
+        
+        storedEvent = localStorage.getItem("storedEvent") || [];
+        
+        // console.log(storedEvent[j].id);
+
     };
 
 };
 
-function init() {
+function timeColor() {
+    
+    for ( var i=9; i<=17; i++) {
 
+        var indexTime = '#' + [i];
+        console.log(i);
+        console.log(moment().format('H'));
+        console.log(indexTime);
+        if ( i < moment().format('H') ) {
+            $(indexTime).addClass('past');
+            $(indexTime).removeClass('future');
+            $(indexTime).removeClass('present');
+        } else if ( i > moment().format('H') ) {
+            $(indexTime).addClass('future');
+            $(indexTime).removeClass('past');
+            $(indexTime).removeClass('present');
+        } else if ( i == moment().format('H') ) {
+            $(indexTime).addClass('present');
+            $(indexTime).removeClass('future');
+            $(indexTime).removeClass('past');
+        };
 
-
+    };
 };
 
-for ( var i=9; i<=16; i++) {
+// for ( var i=9; i<=17; i++) {
 
-    var indexTime = '#' + [i];
+//     var indexTime = '#' + [i];
 
-    $(indexTime).on('click', event => {
-        console.log(event.target);
-      if (event.target.className == 'material-icons unsaved') {
-        console.log('Click!');
-        event.target.className = 'material-icons saved';
-        // save textarea value content to localstorage
-        var storedEvent = $('.description').val() ;
-        localStorage["storedEvent"] = storedEvent ;
-        localStorage.setItem("storedEvent", storedEvent) ;
+//     $(indexTime).on('click', function(event) {
+//         console.log(event.target);
+//       if (event.target.className == 'material-icons unsaved') {
+//         console.log('Click!');
+//         event.target.className = 'material-icons saved';
+//         // save textarea value content to localstorage
 
-      } else if (event.target.className == 'material-icons saved') {
-        console.log('unclick');
-        event.target.className = 'material-icons unsaved';
-        // remove textarea value content from locastorage
-      };
-    });
-};
+//         var newEvent = { 
+//                 eventBlock: $(event.target).parent().siblings().children().val(),
+//                 id: $(event.target).parent().parent().attr('id')
+//             };
+//         console.log(newEvent);
+//         storedEvent.push(newEvent);
+//         localStorage.setItem("storedEvent", JSON.stringify(storedEvent)) ;
+
+//       } else if (event.target.className == 'material-icons saved') {
+//         console.log('unclick');
+//         event.target.className = 'material-icons unsaved';
+//         // remove textarea value content from locastorage
+//       };
+//     });
+// };
 
 init();
